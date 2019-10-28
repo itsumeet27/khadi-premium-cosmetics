884842bec7e215debb85d9f08aa66b90c4eb7c1c
Sumeet Sharma <sksksharma0@gmail.com>
2019-10-28 11:30:43 +0530

    Working on email

--- contact.php
+++ contact.php
@@ -50,6 +50,24 @@
             </div>
           </form>
           <!-- Form contact -->
+          <?php
+
+            if(isset($_POST['submit'])){
+              $ip = getIp();
+              $name = sanitize($_POST['name']);
+              $email = sanitize($_POST['email']);
+              $subject = sanitize($_POST['subject']);
+              $message = sanitize($_POST['message']);
+            }
+            //The code below does not work on localhost but does work on your domain server
+            $from = "itsumeet@gmail.com";
+            $to = $email;
+            $subject = $subject;
+            $body = "Hello $name! Thanks for the enquiry. We will get back to you soon";
+            $headers = "From: $from";
+            mail($to,$subject,$body,$headers);
+            
+          ?>
         </div>
       </div>
       <div class="col-lg-7 col-md-12">
