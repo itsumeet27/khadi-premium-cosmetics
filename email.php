a6ac7a6eb322c9666f6f5e0feedf28f77e56da37
Sumeet Sharma <sksksharma0@gmail.com>
2019-10-28 11:39:16 +0530

    Working on email

--- email.php
+++ email.php
@@ -3,11 +3,21 @@
 
   <div class="text-center container-fluid">
     <?php
-      $from = "sksksharma0@gmail.com";
-      $to = "sksharma.sharma87@gmail.com";
-      $subject = "Email trial";
-      $body = "You have a mail, please check!";
+      if(isset($_POST['submit'])){
+        $ip = getIp();
+        $name = sanitize($_POST['name']);
+        $email = sanitize($_POST['email']);
+        $subject = sanitize($_POST['subject']);
+        $message = sanitize($_POST['message']);
+      }
+      //The code below does not work on localhost but does work on your domain server
+      $from = "itsumeet@gmail.com";
+      $to = $email;
+      $subject = $subject;
+      $body = "Hello $name! Thanks for the enquiry. We will get back to you soon";
       $headers = "From: $from";
+      mail($to,$subject,$body,$headers);
+            
 
       if(mail($to,$subject,$body,$headers)){
         echo "<h2 class='text-success p-3 h2-responsive'>Email sent successfully!</h2>";
